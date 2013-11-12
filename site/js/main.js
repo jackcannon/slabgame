098cd95871dc0db6860a9e15ad561f2d421472b6
Jack Cannon <jack.cannon@holidayextras.com>
2013-11-12 02:02:49 +0000

    preload all images, so theres no ugly bits during play

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,43 @@
+var helper = {
+  capitalise: function capitalise(str) {
+    if(typeof str !== 'string' || str.length == 0) return str;
+    if(str.length == 1) return s.toUpperCase();
+    return str.substr(0,1).toUpperCase() + str.substr(1).toLowerCase();
+  },
+  preload: function preloadImages(arr) {
+    if(!(arr instanceof Array)) return [];
+    var imgs = [];
+    for(var i in arr) {
+      var url = arr[i];
+      if(typeof url !== 'string') continue;
+      var img = new Image();
+      img.src = url;
+
+      imgs.push(img);
+    }
+    return imgs;
+  }
+};
+
 (function () {
+  var images = [
+    './img/apprentice.png',
+    './img/beam.png',
+    './img/bg.png',
+    './img/block.png',
+    './img/football.png',
+    './img/pillar.png',
+    './img/shine-green.png',
+    './img/shine-red.png',
+    './img/shine-white.png',
+    './img/slab.png',
+    './img/speech.png',
+    './img/thought.png',
+    './img/witt.png'
+  ];
+  helper.preload(images);
+
+
   var options = ['block', 'pillar', 'slab', 'beam'];
   var correctAnswer = '';
   var canClick = true;
@@ -39,14 +78,6 @@
       $('#thoughtBubble').show();
       round++;
     }, 1000);
-  }
-
-  var helper = {
-    capitalise: function capitalise(str) {
-      if(typeof str !== 'string' || str.length == 0) return str;
-      if(str.length == 1) return s.toUpperCase();
-      return str.substr(0,1).toUpperCase() + str.substr(1).toLowerCase();
-    }
   }
 
   setup();
